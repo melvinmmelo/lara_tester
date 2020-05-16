d7a25d80942e83c498baccf59e558019b37c8dd5
binvin08 <melomelvin0821@gmail.com>
2020-05-16 22:21:53 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## About this app
+
+This is where I study laravel.
+
+_ 5/16/2020 _
+
+1. Authentication
+2. Vue
+3. File Storage
---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('secret'),
+        ]);
+    }
+}
